893de8b144e702ecdd72e8d2a61eccc5c7ffb76c
ChSonnabend <sonnabendch@gmail.com>
2024-03-13 15:16:11 +0100

    Adding osx support

--- pytorch.sh
+++ pytorch.sh
@@ -8,8 +8,18 @@ prepend_path:
   # For C++ bindings.
   CMAKE_PREFIX_PATH: "$PYTORCH_ROOT/share/cmake"
 ---
+
+case $ARCHITECTURE in
+  *_x86-64) URL=https://download.pytorch.org/libtorch/cpu/libtorch-macos-x86_64-2.2.1.zip
+  ;;
+  *_arm64) URL=https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-2.2.1.zip
+  ;;
+  *) URL=https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.2.1%2Bcpu.zip
+  ;;
+esac
+
 #!/bin/bash -e
-curl -fSsLo pytorch.zip "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.2.1%2Bcpu.zip"
+curl -fSsLo pytorch.zip $URL
 unzip -o pytorch.zip -d "$INSTALLROOT"
 mv "$INSTALLROOT/libtorch"/* "$INSTALLROOT/"
 rmdir "$INSTALLROOT/libtorch"
@@ -17,3 +27,9 @@ rmdir "$INSTALLROOT/libtorch"
 # Modulefile
 mkdir -p "$INSTALLROOT/etc/modulefiles"
 alibuild-generate-module --lib > "$INSTALLROOT/etc/modulefiles/$PKGNAME"
+
+
+case $ARCHITECTURE in
+  osx_*) install_name_tool -add_rpath "$(brew --prefix libomp)/lib" "$INSTALLROOT/lib/libtorch_cpu.dylib"
+  ;;
+esac
