f1f2779896075a8b0cddcdafe8beda1b365fae44
Christian Sonnabend <sonnabendch@gmail.com>
2024-06-28 14:22:53 +0200

    Added gcc 13.2 in o2 and o2-epn recipes. --defaults o2-epn somehow not working(?)

--- defaults-o2-epn.sh
+++ defaults-o2-epn.sh
@@ -29,8 +29,8 @@ overrides:
       - ZeroMQ
       - JAliEn-ROOT
   GCC-Toolchain:
-    version: v12.2.0-alice1
-    tag: v12.2.0-alice1
+    version: v13.2.0-alice1
+    tag: v13.2.0-alice1
   cgal:
     version: 4.12.2
   fastjet:
